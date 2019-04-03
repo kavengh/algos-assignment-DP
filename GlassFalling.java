4e90e3928f2a4baf32cfeab8c7f1ebb7e8ee187d
Kaven Vohra <vohrakaven@gmail.com>
2019-04-02 23:35:15 -0400

    Project was done in external code editor, pushing final project

--- GlassFalling.java
+++ GlassFalling.java
@@ -1,38 +1,177 @@
 /**
- * Glass Falling
+ * Glass Falling:
+ * You are the designer and inventor of a shock-proof landing mat that could catch the fall of glass from a certain height.
+ * You want to guarantee that up to a certain number of floors, the glass will not break.
+ * You need to calculate that highest floor X where the glass will not break, but you only have a set amount of glass panes.
  */
 public class GlassFalling {
 
-  // Do not change the parameters!
-  public int glassFallingRecur(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
-  }
+	/**
+	 * Function that calculates the minimum number of trials it requires to determine
+	 * if a sheet of glass will break when dropped
+	 * @param floors - number of floors
+	 * @param sheets - number of sheets of glass
+	 * @return	the minimum number of trials
+	 */
+	public int glassFallingRecur(int floors, int sheets) {
 
-  // Optional:
-  // Pick whatever parameters you want to, just make sure to return an int.
-  public int glassFallingMemoized() {
-    // Fill in here and change the return
-    return 0;
-  }
+		//base case, when the numbers of floors are equal to 0 or 1
+		if(floors == 1 || floors == 0) {
+			return floors;
+		}
 
-  // Do not change the parameters!
-  public int glassFallingBottomUp(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
-  }
+		//now that the base case for floors is made we need to make the base case for the number of sheets
+		if(sheets == 1) {
+			return floors;
+		}
+
+		//all base cases were made now we have to decide how to solve the problem
+
+		//we set the minimum to the max value to start with
+		int minimumTrials  = Integer.MAX_VALUE;
+		int result;
+
+		//loop through all of the floors starting from the 2nd floor
+		for(int currentFloor = 2; currentFloor <= floors ; currentFloor++) {
+
+			result = Math.max(glassFallingRecur(currentFloor - 1, sheets - 1), glassFallingRecur(floors - currentFloor, sheets));
+
+			if(result < minimumTrials) {
+				minimumTrials = result;
+			}
+		}
+		return minimumTrials + 1;
+	}
 
 
-  public static void main(String args[]){
-      GlassFalling gf = new GlassFalling();
+	//function that returns the max between two numbers
+	public static int max(int num1, int num2) {
+		if( num1 > num2) {
+			return num1;
+		}else {
+			return num2;
+		}
 
-      // Do not touch the below lines of code, and make sure
-      // in your final turned-in copy, these are the only things printed
-      int minTrials1Recur = gf.glassFallingRecur(27, 2);
-      int minTrials1Bottom = gf.glassFallingBottomUp(27, 2);
-      int minTrials2Recur = gf.glassFallingRecur(100, 3);
-      int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
-      System.out.println(minTrials1Recur + " " + minTrials1Bottom);
-      System.out.println(minTrials2Recur + " " + minTrials2Bottom);
-  }
+	}
+
+
+
+	// Optional:
+	// Pick whatever parameters you want to, just make sure to return an int.
+
+	/**
+	 * Function stores the values of previous trials allowing us to solve the problem much faster
+	 * we trade space for time
+	 * @param floors
+	 * @param sheets
+	 * @return The minimum number of trials needed to find what floor the sheet will break on.
+	 */
+	public int glassFallingMemoized(int floors, int sheets) {
+
+		//create a 2D array that will hold the answers to each trial
+		int[][] memo= new int [floors +1][sheets +1];
+
+		int result;
+
+
+		//the base case of when we have one floor or 0 floors still holds
+		for(int k =1; k<=sheets; k++) {
+			memo[0][k] = 1;
+			memo[1][k] = 0;
+			//doesn't matter how many sheets we have when the floor is one or 0 we fill the array with the number of the floors
+		}
+
+		//we also have the base case for when the number of sheets is equal to 1
+		for(int f = 1; f<= floors; f++) {
+			memo[f][1] = f;
+			//since we only have one sheet we have to check all of the floors
+		}
+
+		//now that all of the base cases are done we can calculate all of the trials
+
+		/*
+		 * we need 3 for loops, the 1st and second loop will iterate through out 2D array that
+		 * is holding all of the calculations so far for the floors and the sheets, and the third loop will do all of the
+		 * calculations for the current iteration
+		 */
+
+		for(int currentFloor = 2; currentFloor <= floors; currentFloor++) {
+
+			for(int currentNumSheet = 2; currentNumSheet <= sheets; currentNumSheet++) {
+
+				//first we set the value of each position to the max
+				memo[currentFloor][currentNumSheet] = Integer.MAX_VALUE;
+
+				for( int subProblemFloor = 2; subProblemFloor <= currentFloor; subProblemFloor++ ) {
+					//result = 1 + max(memo[numfloor - 1][ x - 1], memo[numfloor - x][sheets]);
+
+					result = 1+  max(memo[subProblemFloor - 1][ currentNumSheet - 1], memo[currentFloor - subProblemFloor][ currentNumSheet]);
+
+
+					if(result < memo[currentFloor][currentNumSheet]) {
+						memo[currentFloor][currentNumSheet] = result;
+					}
+				}
+			}
+
+		}
+
+
+		//This is the last slot of the table which is the minimum trials
+		return memo[floors][sheets];
+	}
+
+	// Do not change the parameters!
+
+	/**
+	 * This function solves the minimum number of trials starting from the bottom. This is similar to the
+	 * recursive solution however,
+	 * @param floors
+	 * @param sheets
+	 * @return The minimum number of trials needed to find what floor the sheet will break on.
+	 */
+	public int glassFallingBottomUp(int floors, int sheets) {
+		//base case, when the numbers of floors are equal to 0 or 1
+		if(floors == 1 || floors == 0) {
+			return floors;
+		}
+
+		//now that the base case for floors is made we need to make the base case for the number of sheets
+		if(sheets == 1) {
+			return floors;
+		}
+
+		//all base cases were made now we have to decide how to solve the problem
+		int minimumTrails  = Integer.MAX_VALUE; //by default set the min to worst case
+		int result;
+
+		//loop through all of the floors starting from the 2nd floor
+		for(int currentFloor = floors; currentFloor >= 2 ; currentFloor--) {
+
+			result = Math.max(glassFallingRecur(currentFloor - 1, sheets - 1), glassFallingRecur(floors - currentFloor, sheets));
+
+			if(result < minimumTrails) {
+
+				minimumTrails = result;
+			}
+
+		}
+		return minimumTrails + 1;
+	}
+
+	public static void main(String args[]){
+
+		 GlassFalling gf = new GlassFalling();
+
+	     // Do not touch the below lines of code, and make sure
+	     // in your final turned-in copy, these are the only things printed
+	     int minTrials1Recur = gf.glassFallingRecur(27, 2);
+	     int minTrials1Bottom = gf.glassFallingBottomUp(27, 2);
+	     int minTrials2Memo = gf.glassFallingMemoized(100, 3);
+	     int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
+	     System.out.println(minTrials1Recur + " " + minTrials1Bottom);
+	     System.out.println(minTrials2Memo + " " + minTrials2Bottom);
+
+	}
+
 }
--- RodCutting.java
+++ RodCutting.java
@@ -1,33 +1,96 @@
 /**
- * Rod cutting problem described in Chapter 15 of textbook
+ * Rod cutting problem described in Chapter 15 of textbook.
+ * Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n.
+ * Determine the maximum value obtainable by cutting up the rod and selling the pieces.
  */
 public class RodCutting {
 
   // Do not change the parameters!
-  public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
-  }
+
+	/**
+	 * Recursively Check and add the maximum value of selling pieces of the rod.
+	 * @param rodLength - The total length of the rod.
+	 * @param lengthPrices - Array that stores the prices the pieces of the rod sell for.
+	 * @return the maximum value of selling the pieces of the rod.
+	 */
+	public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
+
+		//base case if the length is 0
+		if ( rodLength <= 0) {
+			return rodLength;
+		}
+
+		//by default we set the totalPrice to the min value
+		int endPrice = Integer.MIN_VALUE;
+
+		//loop that goes through each possible length until the max and calculates
+		//the maximum profit from selling the pieces of the rod
+		for (int i = 0; i < rodLength; i++) {
+
+			//we take the max between the previous max and the max of the the previous piece and the new piece
+			endPrice = Math.max(endPrice, lengthPrices[i] + rodCuttingRecur(rodLength-i-1, lengthPrices));
+		}
+
+		//return total price of all pieces sold
+		return endPrice;
+   	}
 
   // Do not change the parameters!
-  public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
-  }
+
+	/**
+	 * Dynamically Check and add the maximum value of selling pieces of the rod.
+	 * @param rodLength - The total length of the rod.
+	 * @param lengthPrices - Array that stores the prices the pieces of the rod sell for.
+	 * @return the maximum value of selling the pieces of the rod.
+	 */
+	public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
+
+		//we create an array of the size of the rod length
+        int totalPrice[] = new int[rodLength+1];
+
+        //base case if no pieces then equal to 0
+        totalPrice[0] = 0;
 
 
-  public static void main(String args[]){
-      RodCutting rc = new RodCutting();
+        //loop iterates through all possible rod lengths
+        for (int i = 1; i<=rodLength; i++)
+        {
+        	//each time we are setting the end price to the min value
+            int endPrice = Integer.MIN_VALUE;
 
-      // In your turned in copy, do not touch the below lines of code.
-      // Make sure below is your only output.
-      int length1 = 7;
-      int[] prices1 = {1, 4, 7, 3, 19, 5, 12};
-      int length2 = 14;
-      int[] prices2 = {2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26};
-      int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
-      int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
-      int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
-      int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
-      System.out.println(maxSell1Recur + " " + maxSell1Bottom);
-      System.out.println(maxSell2Recur + " " + maxSell2Bottom);
-  }
-}
+            //we set the total price to the max between the previous total price and
+            //the total price including the new piece
+            for (int j = 0; j < i; j++) {
+
+                endPrice = Math.max(endPrice, lengthPrices[j] + totalPrice[i-j-1]);
+
+            }
+
+            //before checking the next rod length we store this value that way we dont have to
+            //compute the total of the previous peices again.
+            totalPrice[i] = endPrice;
+        }
+
+        //we return the total price of all the pieces
+        return totalPrice[rodLength];
+	}
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[]){
+        RodCutting rc = new RodCutting();
+
+        // In your turned in copy, do not touch the below lines of code.
+        // Make sure below is your only output.
+        int length1 = 7;
+        int[] prices1 = {1, 4, 7, 3, 19, 5, 12};
+        int length2 = 14;
+        int[] prices2 = {2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26};
+        int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
+        int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
+        int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
+        int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
+        System.out.println(maxSell1Recur + " " + maxSell1Bottom);
+        System.out.println(maxSell2Recur + " " + maxSell2Bottom);
+    }
+} 
